c997a8c9c93be35190dfb3698f3985cbc61a9ff1
Daniel Silva <dan13silv@gmail.com>
2019-05-09 18:12:40 +0000

    Finished with logic and a bit more styling

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     function displayPhotos(data) {
       let photoHTML = '<ul>';
       $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<li class="photo-element">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       }); // end each
